838c1343fa40652fe66e39b2bbb0f307054065b3
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-16 00:54:07 +0530

    Create ADDNATRL.py

---
+++ DSAseries/ADDNATRL.py
@@ -0,0 +1,13 @@
+def sumn(n):
+    if n==0:
+        return n
+    else:
+        return n+sumn(n-1)
+
+if __name__=="__main__":
+    n=int(input())
+    k=sumn(n)
+    print(k)
+    
+   
+  USELESS NZEC ERROR WITH PYTHON CODE . USE THE SECOND APPROACH TO AC ALL TESTCASES.
